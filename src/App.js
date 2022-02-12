86666be6230d0dc9354d94562ccbf59317cf1ee4
maiya-22 <mid.june.light@gmail.com>
2022-02-12 06:31:59 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header"></div>
+      <div className="Board"></div>
+      <div className="Keys"></div>
+    </div>
+  );
+}
+
+export default App;
