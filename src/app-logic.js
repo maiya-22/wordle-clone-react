255c4a3020be5490089db19745f7b89d0c764c0b
maiya-22 <mid.june.light@gmail.com>
2022-02-13 07:13:28 -0500

    refactor for readability

--- src/app-logic.js
+++ src/app-logic.js
@@ -20,7 +20,6 @@ const isRoundOver = (params) => {
 const isLetterInRound = (params) => {
   let { state, board, guess } = params;
   let round = board[state.round];
-
   return round.includes(guess.letter);
 };
 
@@ -29,7 +28,6 @@ const updateBoardRowStatuses = (params) => {
   let { word, round } = params;
 
   return round.map((guess, i) => {
-    let status;
     if (guess.letter === word[i]) {
       guess.status = "exact"; // exact match
     } else if (word.split("").includes(guess.letter)) {
@@ -66,19 +64,20 @@ const isInWordList = (params) => {
 // they keys change color, but maintain status from previous rounds
 const updateKeyboardGuessStatuses = (params) => {
   let { keyboard, round } = params;
-
+  // round in this is the array/row
+  // has the guesses played in this round, and their status
   let lettersInRound = getLettersInRoundHash({ round });
+  // update they keyboard keys' statuses:
   return [...keyboard].map((row) => {
     return row.map((guess) => {
-      let { letter, status } = guess;
       let isLetterInRound = !!lettersInRound[guess.letter];
       if (!isLetterInRound) {
         // letter was not played this round, do nothing
-      } else if (status === "exact") {
+      } else if (guess.status === "exact") {
         // a previous match was as close, or closer than current match, do nothing
       } else if (lettersInRound[letter].status === "exact") {
         guess.status = "exact";
-      } else if (status === "almost") {
+      } else if (guess.status === "almost") {
         // previous match was as close, or closer than current match, do nothing
       } else if (lettersInRound[letter].status === "almost") {
         guess.status = "almost";
