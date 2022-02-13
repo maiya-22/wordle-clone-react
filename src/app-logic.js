a4b3f139c04b51d384ac27f44c7e2d473b6adf21
maiya-22 <mid.june.light@gmail.com>
2022-02-13 12:47:51 -0500

    refactor

--- src/app-logic.js
+++ src/app-logic.js
@@ -3,21 +3,6 @@ import words from "./data";
 // make a hash out of the letters played in the last round, to easily get their status
 // eg { "s": { letter: "s", status: "exact"}} for letters in round
 
-// the board squares change color based on this round's guess
-const updateBoardRowStatuses = (params) => {
-  let { word, row } = params;
-  return row.map((guess, i) => {
-    if (guess.letter === word[i]) {
-      guess.status = "exact"; // exact match
-    } else if (word.split("").includes(guess.letter)) {
-      guess.status = "almost"; // letter is in the word, but not at that position
-    } else {
-      guess.status = "no-match"; // not a match
-    }
-    return guess;
-  });
-};
-
 const getWordFromRow = (params) => {
   let { row } = params;
   return row
@@ -43,23 +28,23 @@ const getLettersInRowHash = (params) => {
 
 // they keys change color, but maintain status from previous rounds
 const updateKeyboardGuessStatuses = (params) => {
-  let { keyboard, round, row } = params;
+  let { keyboard, row } = params;
   // round in this is the array/row
   // has the guesses played in this round, and their status
-  let lettersInRound = getLettersInRowHash({ row });
+  let lettersInRow = getLettersInRowHash({ row });
   // update they keyboard keys' statuses:
   return [...keyboard].map((row) => {
     return row.map((guess) => {
-      let isLetterInRound = !!lettersInRound[guess.letter];
-      if (!isLetterInRound) {
+      let isLetterInRow = !!lettersInRow[guess.letter];
+      if (!isLetterInRow) {
         // letter was not played this round, do nothing
       } else if (guess.status === "exact") {
         // a previous match was as close, or closer than current match, do nothing
-      } else if (lettersInRound[guess.letter].status === "exact") {
+      } else if (lettersInRow[guess.letter].status === "exact") {
         guess.status = "exact";
       } else if (guess.status === "almost") {
         // previous match was as close, or closer than current match, do nothing
-      } else if (lettersInRound[guess.letter].status === "almost") {
+      } else if (lettersInRow[guess.letter].status === "almost") {
         guess.status = "almost";
       } else {
         guess.status = "no-match";
@@ -86,6 +71,21 @@ const isRowComplete = (params) => {
   return row.reduce((isComplete, guess) => {
     return isComplete && !!guess.letter;
   }, true);
+};
+
+// the board squares change color based on this round's guess
+const updateBoardRowStatuses = (params) => {
+  let { word, row } = params;
+  return row.map((guess, i) => {
+    if (guess.letter === word[i]) {
+      guess.status = "exact"; // exact match
+    } else if (word.split("").includes(guess.letter)) {
+      guess.status = "almost"; // letter is in the word, but not at that position
+    } else {
+      guess.status = "no-match"; // not a match
+    }
+    return guess;
+  });
 };
 
 export {
